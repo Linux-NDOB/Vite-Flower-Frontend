567b3b16b714811d8c3eb6767810d618962004aa
Linux-NDOB <lordndob@gmail.com>
2023-01-31 01:59:27 +0000

    finished

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+
+<script setup>
+import Ibar from "/src/components/Navbar.vue";
+import Footer from "/src/components/Footer.vue";
+import Vlogin from '/src/components/Vlogin.vue';
+import { onMounted } from "vue";
+
+onMounted(() => {
+  M.AutoInit();
+});
+</script>
+
+<template>
+  <Ibar />
+  <div class="container">
+    <br />
+    <div class="row">
+      <div class="col s12">
+        <ul class="">
+          <li class=" col s12 indigo darken-4 ">
+            <h5 class="white-text" href="#test1">INICIO DE SESION</h5>
+          </li>
+         </ul>
+      </div>
+
+      <div id="test1" class="col s12">
+        <Vlogin />
+      </div>
+      
+    </div>
+  </div>
+  <Footer />
+</template>
+
